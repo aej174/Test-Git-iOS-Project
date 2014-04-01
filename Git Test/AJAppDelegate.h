4ea24d326f46ad70e930abf2b88733840c4d722c
Allan Jones <allanjones@Macintosh-3.local>
2014-04-01 15:45:39 -0400

    Initial Commit

---
+++ Git Test/AJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AJAppDelegate.h
+//  Git Test
+//
+//  Created by Allan Jones on 4/1/14.
+//  Copyright (c) 2014 Allan Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/AJViewController.h
@@ -0,0 +1,13 @@
+//
+//  AJViewController.h
+//  Git Test
+//
+//  Created by Allan Jones on 4/1/14.
+//  Copyright (c) 2014 Allan Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJViewController : UIViewController
+
+@end
