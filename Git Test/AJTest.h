3e324118cd6affe14f2ef473b1e7d3b36d55ca1a
aej174 <aej174@icloud.com>
2014-04-05 13:41:53 -0400

    Added files "Test"
    New files AJTest.h and AJTest.m

---
+++ Git Test/AJTest.h
@@ -0,0 +1,13 @@
+//
+//  AJTest.h
+//  Git Test
+//
+//  Created by Allan Jones on 4/5/14.
+//  Copyright (c) 2014 Allan Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AJTest : NSObject
+
+@end
---
+++ Git Test/AJTest.m
@@ -0,0 +1,13 @@
+//
+//  AJTest.m
+//  Git Test
+//
+//  Created by Allan Jones on 4/5/14.
+//  Copyright (c) 2014 Allan Jones. All rights reserved.
+//
+
+#import "AJTest.h"
+
+@implementation AJTest
+
+@end
